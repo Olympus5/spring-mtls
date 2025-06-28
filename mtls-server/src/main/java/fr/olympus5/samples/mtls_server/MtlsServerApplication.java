6e614313cedaec0a1d1166fabcfda6df3cdd6878
olympus5 <erwan.iquel@gmail.com>
2025-06-28 23:44:30 +0200

    initialize mtls server

---
+++ mtls-server/src/main/java/fr/olympus5/samples/mtls_server/MtlsServerApplication.java
@@ -0,0 +1,13 @@
+package fr.olympus5.samples.mtls_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtlsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtlsServerApplication.class, args);
+	}
+
+}
---
+++ mtls-server/src/test/java/fr/olympus5/samples/mtls_server/MtlsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.olympus5.samples.mtls_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtlsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
