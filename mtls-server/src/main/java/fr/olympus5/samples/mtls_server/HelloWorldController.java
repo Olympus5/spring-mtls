5d66c5266b9fc47fa895fcbbd4a5b4268ea2f74f
olympus5 <erwan.iquel@gmail.com>
2025-06-30 21:29:36 +0200

    add hello world endpoint

---
+++ mtls-server/src/main/java/fr/olympus5/samples/mtls_server/HelloWorldController.java
@@ -0,0 +1,14 @@
+package fr.olympus5.samples.mtls_server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+    @GetMapping
+    public String world() {
+        return "Hello World!";
+    }
+}
