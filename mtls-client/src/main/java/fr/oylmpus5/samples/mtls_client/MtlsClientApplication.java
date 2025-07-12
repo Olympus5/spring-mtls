2e58ac42e296544217342ddd8560ab0b80d6ce2a
olympus5 <erwan.iquel@gmail.com>
2025-07-12 10:41:52 +0200

    init mtls-client module (and add some configurations to disable useless outputs during test)

---
+++ mtls-client/src/main/java/fr/oylmpus5/samples/mtls_client/MtlsClientApplication.java
@@ -0,0 +1,13 @@
+package fr.oylmpus5.samples.mtls_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtlsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtlsClientApplication.class, args);
+	}
+
+}
---
+++ mtls-client/src/test/java/fr/oylmpus5/samples/mtls_client/MtlsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.oylmpus5.samples.mtls_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtlsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
